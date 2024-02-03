bb862a1795e5021d5b98ffa44717d06791dfeb0c
ikmrn <ibragimovkmrn@gmail.com>
2024-02-03 23:23:49 +0500

    feat: Add increment save and reset functions

--- passenger-counter-app/js/index.js
+++ passenger-counter-app/js/index.js
@@ -11,4 +11,33 @@ document.addEventListener("DOMContentLoaded", function () {
   let saveBtn = document.getElementById("save-btn");
   let countElement = document.getElementById("count");
   let count = 0;
+  
+   // Function to reset the count
+  function reset() {
+    count = 0;
+    countElement.textContent = count;
+  }
+
+  // Function to increment the count
+  function increment() {
+    count++;
+    countElement.textContent = count;
+  }
+
+  // Function to save the current count and append it to the DOM
+  // Then reset the count
+  function save() {
+    var parentElement = document.querySelector("body > div");
+    var newSave = document.createElement("span");
+    newSave.textContent = count;
+    parentElement.appendChild(newSave);
+    console.log(count);
+
+    reset();
+  }
+
+  // Listen for click events on the buttons
+  incrementBtn.addEventListener("click", increment);
+  resetBtn.addEventListener("click", reset);
+  saveBtn.addEventListener("click", save);
 });
