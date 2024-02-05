8b552740afdd0be65ad97458986369b9626c8ce0
ikmrn <ibragimovkmrn@gmail.com>
2024-02-05 17:36:18 +0500

    feat: Add 'newCard' function rewrite 'startGame'

--- blackjack/js/index.js
+++ blackjack/js/index.js
@@ -10,12 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Access elements from the HTML
   let startBtn = document.getElementById("start-btn");
+  let newCardBtn = document.getElementById("newcard-btn");
   let messageEl = document.getElementById("message-el");
   let sumEl = document.getElementById("sum-el");
   let cardsEl = document.getElementById("cards-el");
 
+  let cards = [firstCard, secondCard]
+
   // Function to start the game
-  function startGame() {
+  function renderGame() {
+      cardsEl.textContent = "Cards: "
+      for (let i = 0, n = cards.length; i < n; i++) {
+        cardsEl.textContent += `${cards[i]} `
+      }
+
+
     if (sum < 21) {
       message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -29,8 +38,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Update the message, sum, and cards displayed on the HTML page
     messageEl.textContent = message;
     sumEl.textContent = `Sum: ${sum}`;
-    cardsEl.textContent = `Cards: ${firstCard} ${secondCard} `;
+  } 
+  
+  // Function to draw a new card
+  function newCard() {
+    let card = 6
+    sum += card
+    cards.push(card)
+    renderGame()
   }
-  // Add an event listener to the start button
-  startBtn.addEventListener("click", startGame);
+
+
+  // Add an event listener to the buttons
+  startBtn.addEventListener("click", renderGame);
+  newCardBtn.addEventListener("click", newCard);
 });
