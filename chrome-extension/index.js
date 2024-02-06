e3ce00855a9076e7b4eb34b25c3812309e6db91e
ikmrn <ibragimovkmrn@gmail.com>
2024-02-06 16:31:55 +0500

    feat: Add 'renderLeads' function

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -3,23 +3,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEl = document.getElementById("input-el");
   const inputBtn = document.getElementById("input-btn");
   const ulEl = document.getElementById("ul-el");
-  const li = document.createElement("li");
-  let myLeads = [];
+
+  // Retrieve leads from local storage
+  let myLeads = JSON.parse(localStorage.getItem("myLeads")) || [];
 
   function saveLead() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    listItems = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    renderLeads();
+  }
 
+  function renderLeads() {
+    let listItems = "";
     myLeads.forEach((element) => {
-      listItems += `<li><a href="${element}" target="blank">${element}</a></li>`;
+      listItems += `
+        <li>
+          <a href="${element}" target="_blank">
+            ${element}
+          </a>
+        </li>`;
     });
     ulEl.innerHTML = listItems;
   }
+
   inputBtn.addEventListener("click", saveLead);
   inputEl.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
       saveLead();
     }
   });
+  
+
+  // Initial rendering of leads
+  renderLeads();
 });
