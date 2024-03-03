b651874294cd1aa1971f80c7d6685387c6f03f72
ikmrn <ibragimovkmrn@gmail.com>
2024-03-03 22:06:44 +0500

    feat: Add HTML elements references

--- product-filter/script.js
+++ product-filter/script.js
@@ -2,73 +2,86 @@ const products = [
   {
     name: "Sony Playstation 5",
     url: "images/playstation_5.png",
-    type: "games",
+    category: "games",
     price: 499.99,
   },
   {
     name: "Samsung Galaxy",
     url: "images/samsung_galaxy.png",
-    type: "smartphones",
+    category: "smartphones",
     price: 399.99,
   },
   {
     name: "Cannon EOS Camera",
     url: "images/cannon_eos_camera.png",
-    type: "cameras",
+    category: "cameras",
     price: 749.99,
   },
   {
     name: "Sony A7 Camera",
     url: "images/sony_a7_camera.png",
-    type: "cameras",
+    category: "cameras",
     price: 1999.99,
   },
   {
     name: "LG TV",
     url: "images/lg_tv.png",
-    type: "televisions",
+    category: "televisions",
     price: 799.99,
   },
   {
     name: "Nintendo Switch",
     url: "images/nintendo_switch.png",
-    type: "games",
+    category: "games",
     price: 299.99,
   },
   {
     name: "Xbox Series X",
     url: "images/xbox_series_x.png",
-    type: "games",
+    category: "games",
     price: 499.99,
   },
   {
     name: "Samsung TV",
     url: "images/samsung_tv.png",
-    type: "televisions",
+    category: "televisions",
     price: 1099.99,
   },
   {
     name: "Google Pixel",
     url: "images/google_pixel.png",
-    type: "smartphones",
+    category: "smartphones",
     price: 499.99,
   },
   {
     name: "Sony ZV1F Camera",
     url: "images/sony_zv1f_camera.png",
-    type: "cameras",
+    category: "cameras",
     price: 799.99,
   },
   {
     name: "Toshiba TV",
     url: "images/toshiba_tv.png",
-    type: "televisions",
+    category: "televisions",
     price: 499.99,
   },
   {
     name: "iPhone 14",
     url: "images/iphone_14.png",
-    type: "smartphones",
+    category: "smartphones",
     price: 999.99,
   },
 ];
+
+const productsWrapper = document.getElementById("products-wrapper");
+const checkboxes = document.querySelectorAll(".check");
+const filterContainer = document.getElementById("filter-container");
+const searchInput = document.getElementById("search");
+const cartCount = document.getElementById("cart-count");
+
+// Init cart item count
+let cartItemCount = 0;
+
+// Init product element array
+const productElements = [];
+
